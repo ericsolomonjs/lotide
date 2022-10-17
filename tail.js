5f64ed6f3d353681c516882205545c7f00ac7d00
facelessagony <eric.solomon.ca@gmail.com>
2022-10-17 22:46:38 +0000

    added tail.js file

---
+++ tail.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed : ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed : ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  let lclTail = [];
+  for (let i = 1; i < array.length; i++) {
+    lclTail[i] = array[i];
+  }
+  return lclTail;
+};
