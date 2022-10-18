23bed32f5f90be14699319bb432dc3316ae21ff2
facelessagony <eric.solomon.ca@gmail.com>
2022-10-18 23:43:37 +0000

    Added assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0 ; i < array1.length ; i++ ) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  } else {return false;}
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`Assertion failed: ${array1} !== ${array2}`);
+  }
+}
