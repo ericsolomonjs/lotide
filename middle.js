4dece4652f886c8f564676811c6b29e9040b9351
facelessagony <eric.solomon.ca@gmail.com>
2022-10-24 18:50:33 +0000

    middle refactor and test file

--- middle.js
+++ middle.js
@@ -1,21 +1,6 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length === array2.length) {
-    for (let i = 0 ; i < array1.length ; i++ ) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-  } else {return false;}
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`Assertion passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`Assertion failed: ${array1} !== ${array2}`);
-  }
-}
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function (array1) {
   if (array1.length < 3) {
@@ -27,11 +12,4 @@ const middle = function (array1) {
   }
 };
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), [] ); // => []
-
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3] ); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]) , [3, 4])// => [3, 4]
+module.exports = middle;
