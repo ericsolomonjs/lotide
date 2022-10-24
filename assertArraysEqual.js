c5cb9ed9371886e2926eac9a45ae31bc8dcfc3f7
facelessagony <eric.solomon.ca@gmail.com>
2022-10-24 18:36:33 +0000

    tail.js & test bugfix

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,4 @@
-const eqArrays = require("eqArrays");
+const eqArrays = require("./eqArrays");
 
 
 const assertArraysEqual = function(array1, array2) {
@@ -9,4 +9,4 @@ const assertArraysEqual = function(array1, array2) {
   }
 }
 
-module.export = assertArraysEqual;
+module.exports = assertArraysEqual;
